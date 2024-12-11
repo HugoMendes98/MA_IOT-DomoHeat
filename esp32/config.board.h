3508df9e0b939463c490edb125480c062137636a
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-12-11 17:30:32 +0100

    chore: ok esp with MQTT

---
+++ esp32/config.board.h
@@ -0,0 +1,14 @@
+// Board configuration
+// Avoid to change, unless experienced/for demo/test/...
+
+// The number means when the temperature is set to > X (ex: > 10)
+const int PIN_LED_10 = 14;
+const int PIN_LED_20 = 32;
+const int PIN_LED_30 = 15;
+
+// Pins for the BME module
+const int PIN_BME_GND = 19;  // will "work" as GND
+const int PIN_BME_VLT = 16;  // will "work" as the source
+
+const int PIN_BME_CLK = 21;
+const int PIN_BME_DATA = 17;
---
+++ esp32/esp32.ino
@@ -0,0 +1,159 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <Wire.h>
+
+// https://github.com/malokhvii-eduard/arduino-bme280 (available in the library manager)
+#include <Bme280.h>
+// https://github.com/256dpi/arduino-mqtt (available in the library manager)
+#include <MQTT.h>
+// https://arduinojson.org/?utm_source=meta&utm_medium=library.properties (available in the library manager)
+#include <ArduinoJson.h>
+
+#include "./config.h"
+#include "./config.board.h"
+#include "./wifi.h"
+
+Bme280TwoWire bme;
+
+WiFiClient net; // This must be global-scoped
+MQTTClient mqttClient(4096); // 4kB should be enough
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  JsonDocument doc;
+  auto error = deserializeJson(doc, payload);
+  if (error) {
+    Serial.println("Could not JSON deserialize");
+    return;
+  }
+  
+  if (!doc["temp"].is<double>()) {
+    Serial.println("'temp' not found in JSON");
+    return;
+  }
+
+  double temp = doc["temp"];
+  Serial.print("Received 'temp' of: ");
+  Serial.println(temp);
+
+  digitalWrite(PIN_LED_10, LOW);
+  digitalWrite(PIN_LED_20, LOW);
+  digitalWrite(PIN_LED_30, LOW);
+
+  if (temp < 10) {
+    return;
+  }
+
+  digitalWrite(PIN_LED_10, HIGH);
+  if (temp < 20) {
+    return;
+  }
+  
+  digitalWrite(PIN_LED_20, HIGH);
+  if (temp < 30) {
+    return;
+  }
+
+  digitalWrite(PIN_LED_30, HIGH);
+
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+}
+
+
+void setup() {
+  // Some boards work best if we also make a serial connection
+  Serial.begin(115200);
+  while(!Serial) {}
+
+  pinMode(PIN_LED_10, OUTPUT);
+  pinMode(PIN_LED_20, OUTPUT);
+  pinMode(PIN_LED_30, OUTPUT);
+
+  pinMode(PIN_BME_GND, OUTPUT);
+  pinMode(PIN_BME_VLT, OUTPUT);
+
+  // Work as the alimentation of the module
+  digitalWrite(PIN_BME_VLT, LOW);
+  digitalWrite(PIN_BME_VLT, HIGH);
+
+  // https://github.com/malokhvii-eduard/arduino-bme280/blob/master/examples/TwoWire/TwoWire.ino
+  Wire.begin(PIN_BME_DATA, PIN_BME_CLK);
+  bme.begin(Bme280TwoWireAddress::Primary);
+
+  // WIFI - https://docs.arduino.cc/retired/library-examples/wifi-library/ConnectWithWPA/
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  WiFi.mode(WIFI_STA);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(WIFI_SSID);
+
+    // Connect to WPA/WPA2 network:
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+    // wait 2.5 seconds (for connection and retry)
+    delay(2500);
+  }
+
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+
+  // MQTT - https://github.com/256dpi/arduino-mqtt
+  Serial.print("Connecting to MQTT at ");
+  Serial.print(MQTT_HOST);
+  Serial.print(":");
+  Serial.print(MQTT_PORT);
+  
+  mqttClient.begin(MQTT_HOST, MQTT_PORT, net);
+  mqttClient.onMessage(messageReceived);
+
+  Serial.print("\nMQTT connecting...");
+  while (!mqttClient.connect("esp32")) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println("MQTT connected!");
+
+  mqttClient.subscribe("/set");
+}
+
+unsigned long lastMillis = 0;
+void loop() {
+  mqttClient.loop();
+
+  if (millis() - lastMillis < 1000) {
+    return;
+  }
+  lastMillis = millis();
+
+  Serial.print("Temperature = ");
+  Serial.print(bme.getTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.getPressure());
+  Serial.println(" hPa");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.getHumidity());
+  Serial.println(" %");
+
+  JsonDocument doc;
+  doc["temp"] = bme.getTemperature();
+  String output;
+  serializeJson(doc, output);
+
+  mqttClient.publish("/sync", output);
+}
